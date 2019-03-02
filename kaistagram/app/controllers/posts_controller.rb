4185c2a143a8f8654def96869a9dd34d338fb467
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-03-02 22:37:15 +0000

    main index page after logging in has image upload form

--- kaistagram/app/controllers/posts_controller.rb
+++ kaistagram/app/controllers/posts_controller.rb
@@ -6,4 +6,14 @@ class PostsController < ApplicationController
     @user = current_user.username
   end
 
+  def create
+  Post.create(post_params)
+  redirect_to root_path
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:caption, :images, :user_id)
+  end
+
 end
