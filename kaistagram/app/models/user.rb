3d17e7c9e09e2cdf0ca074aab7f11eb1928a1844
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-03-02 20:39:59 +0000

    Devise accepts username and stores it in database

--- kaistagram/app/models/user.rb
+++ kaistagram/app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_writer :login
+
+  def login
+    @login || username || email
+  end
 end
